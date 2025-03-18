738fc90fbfd3287590706e82c63a1558c9745801
root <root@Jon.>
2025-03-18 12:18:06 -0700

    test 7

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+  const responseObject = {
+    message: "My name is Jonathan",
+    timestamp: Date.now()
+  };
+
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({
-      message: "My name is Jonathan",
-      timestamp: Date.now()
-    }));
+  res.send(JSON.stringify(responseObject)); // Minified JSON response
 });
+
 app.listen(3000);
 
 
