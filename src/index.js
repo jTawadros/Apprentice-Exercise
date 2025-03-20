314ee5b8e7b68933fcf24955f73f195fb997b12f
root <root@Jon.>
2025-03-19 18:14:18 -0700

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const app = express();
 
-let reqCount = 0;
 app.get('/', (req, res) => {
-  reqCount++;
-  if (reqCount === 7) {
-    return res.status(200).send('test');
-  }
   res.json({
     message: "My name is Jonathan",
     timestamp: Date.now()
