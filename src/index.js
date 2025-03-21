52d3c4e21447814d2f8bbc5f0a2c562ad49f2563
root <root@Jon.>
2025-03-20 17:23:51 -0700

    edit json output

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.json({
     message: "My name is Jonathan",
     timestamp: Date.now(),
-    verify: "Confirm Deployment"
+    verify: "Confirm Deployment using github actions on push!"
   });
 });
 
